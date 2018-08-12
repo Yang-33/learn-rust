fafe3c4be5d290599860e416e9ca7966806a05c8
Yang-33 <kasai.yuta0810@gmail.com>
2018-08-12 18:38:32 +0900

    testを確認

---
+++ hello/target/debug/deps/hello-56255bbe7272420e.d
@@ -0,0 +1,5 @@
+/mnt/c/home/rust-learn/hello/target/debug/deps/hello-56255bbe7272420e: src/main.rs
+
+/mnt/c/home/rust-learn/hello/target/debug/deps/hello-56255bbe7272420e.d: src/main.rs
+
+src/main.rs:
