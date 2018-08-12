df4cc821224b3f07e7d40c4bdb56c22040ca9f61
Yang-33 <kasai.yuta0810@gmail.com>
2018-08-12 21:49:18 +0900

    gcd完了

---
+++ hello/target/debug/deps/hello-897c4f48e7485025.d
@@ -0,0 +1,5 @@
+/mnt/c/home/rust-learn/hello/target/debug/deps/hello-897c4f48e7485025: src/main.rs
+
+/mnt/c/home/rust-learn/hello/target/debug/deps/hello-897c4f48e7485025.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/mnt/c/home/rust-learn/hello/target/debug/hello: /mnt/c/home/rust-learn/hello/src/main.rs
