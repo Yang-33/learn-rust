bc18f0d40b8287b4ee00293c539e857b0fda2560
Yang-33 <kasai.yuta0810@gmail.com>
2018-08-14 21:46:11 +0900

    4-1

---
+++ syntax_semantics/src/4-1.rs
@@ -0,0 +1,48 @@
+
+fn mode001(){
+    let x: i32;
+    println!("hello.");
+}
+
+//fn mode002(){ // need to initialize x.
+//    let x: i32;
+//    println!("The value of x is: {}",x);
+//}
+
+fn mode003(){ // initialize
+    let x: i32 = 10;
+    println!("The value of x is: {}",x);
+}
+
+//fn mode004(){// scope
+//    let x:i32 = 17;
+//    {
+//        let y:i32=3;
+//        println!("The value of x is {} and value of y is {}.",x,y);
+//    }
+//    println!("The value of x is {} ans value of y is {}.",x,y);
+//}
+
+fn mode005(){// scope
+    let x:i32 = 17;
+    {
+        let y:i32=3;
+        println!("The value of x is {} and value of y is {}.",x,y);
+    }
+}
+
+fn mode006(){ // shadowing
+    let x:i32 = 8;
+    {
+        println!("{}",x);
+        let x = 14;
+        println!("{}",x);
+    }
+    println!("{}",x);
+    let x = 42;
+    println!("{}",x);
+}
+
+fn main() {
+    mode006();
+}
--- syntax_semantics/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let x= 5;
-    let(x,y) = (1,2);
-
-    let x:i32 = 10;
-}
