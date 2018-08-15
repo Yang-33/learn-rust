7dd8d345f959574783c49774ab36f3ad7a55bf53
Yang-33 <kasai.yuta0810@gmail.com>
2018-08-15 10:00:53 +0900

    4-6

--- syntax_semantics/src/main.rs
+++ syntax_semantics/src/main.rs
@@ -1,66 +1,92 @@
-// 4-5
-// if
+// 4-6
+// loop
 
+
+// 無限ループ
 fn mode001() {
-    let x = 5;
-    if x == 5 {
-        println!("x is 5!");
+    loop {
+        println!("Loop forever!");
     }
 }
 
+// while
 fn mode002() {
-    let x = 3;
-    if x == 5 {
-        println!("x is 5!");
+    let mut x = 5;
+    let mut done = false;
+    while !done {
+        x += x - 3;
+        println!("{}", x);
+
+        if x % 5 == 0 {
+            done = true;
+        }
     }
 }
 
+// 安全性が低い(loop)とは等価に扱われない
 fn mode003() {
-    let x = 5;
-    if x == 5 {
-        println!("x is 5!");
-    } else {
-        println!("x is not 5!");
+    while true {
+        println!("Loop forever!");
     }
 }
+
+// for
 
 fn mode004() {
-    let x = 4;
-    if x == 5 {
-        println!("x is 5!");
-    } else {
-        println!("x is not 5!");
+    for x in 0..10 {
+        println!("x is {}", x);
     }
 }
 
+// enumerate
 fn mode005() {
-    let x = 5;
-    if x == 5 {
-        println!("x is 5!");
-    } else if x == 6 {
-        println!("x is not 6!");
-    } else {
-        println!("e----");
+    for (i, j) in (5..10).enumerate() {
+        println!("i = {}, j = {}", i, j);
     }
 }
 
+// Lines型というものがある
+// enumerateは&strにはなかった
 fn mode006() {
-    let x = 5;
-    let y = if x == 5 {
-        10
-    } else {
-        15
-    };
-    println!("y is: {}", y);
+    let lines = "hello\nworld".lines();
+    for (linenumber, line) in lines.enumerate() {
+        println!("{}: {}", linenumber, line);
+    }
 }
 
-// better
+// 反復の早期終了
 fn mode007() {
-    let x = 6;
-    let y = if x == 6 { 100 } else { 3000 };
-    println!("y is:{}",y);
+    let mut x = 5;
+
+    loop {
+        x += x - 3;
+        println!("{}", x);
+        if x % 5 == 0 {
+            break;
+        }
+    }
+}
+
+fn mode008() {
+    for x in 00..20 {
+        if x % 2 == 0 { continue; }
+        println!("{}", x);
+    }
+}
+
+// loop label
+// 競技的には便利そうだけど一般的にはそこまで便利ではなさそう
+fn mode009() {
+    'outer:
+        for x in 0..10 {
+        'inner: for y in 0..10 {
+            if x % 2 == 0 { continue 'outer; }
+            if y % 2 == 0 { continue 'inner; }
+            println!("x:{}, y:{}", x, y);
+        }
+    }
 }
 
 fn main() {
-    mode007();
+    mode009();
 }
