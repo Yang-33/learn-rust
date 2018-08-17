dfa78bcacc98b04a530b3c63041b28f42282885c
Yang-33 <kasai.yuta0810@gmail.com>
2018-08-17 18:15:46 +0900

    4-34 macro

---
+++ syntax_semantics/src/4-34.rs
@@ -0,0 +1,13 @@
+// 4-34
+// macro
+
+
+fn mode001(){
+ // https://rust-lang-ja.github.io/the-rust-programming-language-ja/1.6/book/macros.html
+    // 結構大変
+    // 必要となったときにやるのが良さそう
+}
+
+fn main(){
+    mode001();
+}
