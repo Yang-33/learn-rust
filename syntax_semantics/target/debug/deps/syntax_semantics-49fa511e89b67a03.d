8fba95e7e15dc8e441ee7b84a84276b0f1eb6157
Yang-33 <kasai.yuta0810@gmail.com>
2018-08-17 16:31:52 +0900

    4-27 attribute

---
+++ syntax_semantics/target/debug/deps/syntax_semantics-49fa511e89b67a03.d
@@ -0,0 +1,5 @@
+/mnt/c/home/rust-learn/syntax_semantics/target/debug/deps/syntax_semantics-49fa511e89b67a03: src/main.rs
+
+/mnt/c/home/rust-learn/syntax_semantics/target/debug/deps/syntax_semantics-49fa511e89b67a03.d: src/main.rs
+
+src/main.rs:
---
+++ syntax_semantics/target/debug/deps/syntax_semantics-c3174fbaba5b1025.d
@@ -0,0 +1,11 @@
+/mnt/c/home/rust-learn/syntax_semantics/target/debug/deps/syntax_semantics-c3174fbaba5b1025: src/lib.rs src/english/mod.rs src/english/greetings.rs src/english/farewells.rs src/japanese/mod.rs src/japanese/greetings.rs src/japanese/farewells.rs
+
+/mnt/c/home/rust-learn/syntax_semantics/target/debug/deps/syntax_semantics-c3174fbaba5b1025.d: src/lib.rs src/english/mod.rs src/english/greetings.rs src/english/farewells.rs src/japanese/mod.rs src/japanese/greetings.rs src/japanese/farewells.rs
+
+src/lib.rs:
+src/english/mod.rs:
+src/english/greetings.rs:
+src/english/farewells.rs:
+src/japanese/mod.rs:
+src/japanese/greetings.rs:
+src/japanese/farewells.rs:
